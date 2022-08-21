644d66f30f69c19232cc2c6a7f5f3f6de8e0b174
KentaKishi888 <kenta.kishi@salto.link>
2022-08-22 08:34:54 +0900

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+//actions
+const increment = () => {
+  return {
+    type:"INCREMENT",
+    
+  }
+}
+const decrement = () => {
+  return {
+    type:"DECREMENT",
+
+  }
+}
+
+
+//reducer
+const counterReducer = (state = 0, aciton) => {
+  switch(aciton.type){
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+}
+
+// store 
+let store = createStore(counterReducer);
+
+// dispatch
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+
+// console 
+store.subscribe(() => console.log(store.getState()));
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +48,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
