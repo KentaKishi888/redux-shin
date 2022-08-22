babbbd073fd2e066453e0c6eca7e43a237edaa48
KentaKishi888 <kenta.kishi@salto.link>
2022-08-22 09:06:47 +0900

    create store&provider

--- src/index.js
+++ src/index.js
@@ -6,15 +6,19 @@ import { createStore } from 'redux';
 import counterReducer from './reducers/counter';
 import isLoginReducer from './reducers/isLogin';
 import allReducers from './reducers';
+import { Provider } from "react-redux";
 
-const store = createStore(allReducers);
+const store = createStore(allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
